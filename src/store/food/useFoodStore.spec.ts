13e89cda61434d04663ecaf70e7471f77518f98b
Guilherme <guilherme.nagasaki@outlook.com>
2023-09-12 17:27:13 -0300

    add stores tests

---
+++ src/store/food/useFoodStore.spec.ts
@@ -0,0 +1,60 @@
+import { renderHook } from '@testing-library/react';
+
+import { useFoodStore } from '.';
+import { act } from 'react-dom/test-utils';
+
+const foodMock1 = {
+  _id: '1',
+  number: 1,
+  description: 'test1',
+  calories: 1,
+  carbs: 1,
+  protein: 1,
+  fat: 1,
+  fiber: 1
+};
+
+jest.mock('@/functions/createId', () => ({
+  createId: jest.fn().mockImplementation(() => '')
+}));
+
+describe('use food store', () => {
+  it('should initial state correct', () => {
+    const { result } = renderHook(() => useFoodStore());
+
+    const initialArray = result.current.state.foods;
+
+    expect(initialArray).toStrictEqual([]);
+  });
+
+  it('should add food working correctly', () => {
+    const { result } = renderHook(() => useFoodStore());
+
+    act(() => {
+      result.current.actions.addFood(foodMock1);
+    });
+
+    const foodArray = result.current.state.foods;
+
+    expect(foodArray).toContainEqual({ ...foodMock1, _id: '' });
+  });
+
+  it('should remove food working correctly', () => {
+    const { result } = renderHook(() => useFoodStore());
+
+    act(() => {
+      result.current.actions.addFood(foodMock1);
+    });
+
+    expect(result.current.state.foods).toContainEqual({
+      ...foodMock1,
+      _id: ''
+    });
+
+    act(() => {
+      result.current.actions.removeFood('');
+    });
+
+    expect(result.current.state.foods).toStrictEqual([]);
+  });
+});
---
+++ src/store/search/useSearchStore.spec.ts
@@ -0,0 +1,46 @@
+import { renderHook } from '@testing-library/react';
+
+import { useSearchStore } from '.';
+import { act } from 'react-dom/test-utils';
+
+describe('use search store', () => {
+  it('should initial state correct', () => {
+    const { result } = renderHook(() => useSearchStore());
+
+    const initialSate = result.current.state.searchFood;
+
+    expect(initialSate).toStrictEqual('');
+  });
+
+  it('should set search food working correctly', () => {
+    const { result } = renderHook(() => useSearchStore());
+
+    const testString = 'test';
+
+    act(() => {
+      result.current.actions.setSearchFood(testString);
+    });
+
+    const searchFoodState = result.current.state.searchFood;
+
+    expect(searchFoodState).toStrictEqual(testString);
+  });
+
+  it('should clear search food working correctly', () => {
+    const { result } = renderHook(() => useSearchStore());
+
+    const testString = 'test';
+
+    act(() => {
+      result.current.actions.setSearchFood(testString);
+    });
+
+    expect(result.current.state.searchFood).toStrictEqual(testString);
+
+    act(() => {
+      result.current.actions.clearSearch();
+    });
+
+    expect(result.current.state.searchFood).toStrictEqual('');
+  });
+});
