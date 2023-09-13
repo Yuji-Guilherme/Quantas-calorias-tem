b1cab5b2ea305e2ad955351bd1293d7d95342b58
Guilherme <guilherme.nagasaki@outlook.com>
2023-09-13 17:26:29 -0300

    add integration test of input

--- src/templates/SearchTemplate/SearchTemplate.test.tsx
+++ src/templates/SearchTemplate/SearchTemplate.test.tsx
@@ -2,9 +2,31 @@ import { fireEvent, render, screen } from '@testing-library/react';
 
 import { SearchTemplate } from '.';
 
+const foodMock1 = {
+  _id: '1',
+  number: 10,
+  description: 'test1',
+  calories: 1,
+  carbs: 1,
+  protein: 1,
+  fat: 1,
+  fiber: 1
+};
+
+const foodMock2 = {
+  _id: '2',
+  number: 20,
+  description: 'test2',
+  calories: 2,
+  carbs: 2,
+  protein: 2,
+  fat: 2,
+  fiber: 2
+};
+
 jest.mock('@/hook/useFetch', () => ({
   useFetch: jest.fn().mockImplementation(() => ({
-    data: [''],
+    data: [foodMock1, foodMock2],
     isLoading: false,
     iseError: false
   }))
@@ -22,5 +44,22 @@ describe('<SearchTemplate>', () => {
     fireEvent.click(showButtonElement);
 
     expect(arrowIconElement).toHaveAttribute('data-show', 'true');
+  });
+
+  it('should focus first menu item with press arrow down in input', () => {
+    render(<SearchTemplate />);
+
+    const inputElement = screen.getByRole('textbox');
+
+    fireEvent.click(inputElement);
+    fireEvent.keyDown(inputElement, {
+      key: 'ArrowDown',
+      code: 'ArrowDown',
+      keyCode: 40
+    });
+
+    const inputMenuOptionELement = screen.getByLabelText(foodMock1.description);
+
+    expect(inputMenuOptionELement).toHaveFocus();
   });
 });
